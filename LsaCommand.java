7317317385465c348fd9627547aa718226d9688a
nouratarek18 <125820745+nouratarek18@users.noreply.github.com>
2024-10-31 14:11:13 +0300

    Update LsaCommand.java

--- LsaCommand.java
+++ LsaCommand.java
@@ -1,21 +1,25 @@
 package org.os;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LsaCommand implements Command {
     @Override
-    public void execute(String[] args) {
+    public String[] execute(String[] args) {
+        List<String> result = new ArrayList<>();
         File currentDir = new File(System.getProperty("user.dir"));
         File[] files = currentDir.listFiles();
 
         if (files == null) {
-            System.out.println("Unable to access directory contents.");
-            return;
+            result.add("Unable to access directory contents.");
+            return result.toArray(new String[0]);
         }
 
         Arrays.stream(files)
-                .forEach(file -> System.out.println(file.getName()));
+                .forEach(file -> result.add(file.getName())); // Collecting all file names
+
+        return result.toArray(new String[0]); // Returning the collected output
     }
 }
-
