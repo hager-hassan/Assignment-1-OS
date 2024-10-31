c6744a7926ffd31030542356de0bbe2837f5bd87
nouratarek18 <125820745+nouratarek18@users.noreply.github.com>
2024-10-31 14:13:13 +0300

    Update LsrCommand.java

--- LsrCommand.java
+++ LsrCommand.java
@@ -1,22 +1,28 @@
 package org.os;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class LsrCommand implements Command {
     @Override
-    public void execute(String[] args) {
+    public String[] execute(String[] args) {
+        List<String> result = new ArrayList<>();
         File currentDir = new File(System.getProperty("user.dir"));
         File[] files = currentDir.listFiles();
 
         if (files == null) {
-            System.out.println("Unable to access directory contents.");
-            return;
+            result.add("Unable to access directory contents.");
+            return result.toArray(new String[0]);
         }
 
         Arrays.stream(files)
-                .sorted(Comparator.reverseOrder())
-                .forEach(file -> System.out.println(file.getName()));
+                .sorted(Comparator.reverseOrder()) // Sort files in reverse order
+                .forEach(file -> result.add(file.getName())); // Collecting all file names
+
+        return result.toArray(new String[0]); // Returning the collected output
     }
 }
+
