a2b34ef23da4cbffe01f12b30541607c7b1efc0f
nouratarek18 <125820745+nouratarek18@users.noreply.github.com>
2024-10-31 16:43:54 +0300

    Update LsrCommand.java

--- LsrCommand.java
+++ LsrCommand.java
@@ -13,6 +13,11 @@ public class LsrCommand implements Command {
         File currentDir = new File(System.getProperty("user.dir"));
         File[] files = currentDir.listFiles();
 
+        if (args.length > 0) {
+            result.add("\u001B[31mError: This command does not accept any arguments.\u001B[0m"); // Red error message
+            return result.toArray(new String[0]);
+        }
+
         if (files == null) {
             result.add("Unable to access directory contents.");
             return result.toArray(new String[0]);
